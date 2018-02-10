b9ed31bee4804a7e3a0da586f0f9731f3831809b
ChristianNavolskyi <c.navolskyi@gmail.com>
2018-02-10 19:16:03 +0100

    Modified to read specific key from file with all entries of one kind.

--- filestore/src/main/java/com/yahoo/ycsb/db/FileStoreClient.java
+++ filestore/src/main/java/com/yahoo/ycsb/db/FileStoreClient.java
@@ -25,11 +25,11 @@ import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.Status;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 import static java.io.File.separatorChar;
@@ -40,6 +40,7 @@ import static java.io.File.separatorChar;
  */
 public class FileStoreClient extends DB {
 
+  public static final String KEY_IDENTIFIER = "Key";
   /**
    * The name and default value of the property for the output directory for the files.
    */
@@ -48,14 +49,12 @@ public class FileStoreClient extends DB {
       + separatorChar
       + "benchmarkingData"
       + separatorChar;
-
   /**
    * The property name to enable pretty printing of the json in created files.
    * This will increase the size of the files substantially!
    */
   private static final String ENABLE_PRETTY_PRINTING = "enablePrettyPrinting";
   private static final String ENABLE_PRETTY_PRINTING_DEFAULT = "false";
-
   private final GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(ByteIterator.class, new
       ByteIteratorAdapter());
   private final Type valuesType = new TypeToken<Map<String, ByteIterator>>() {}.getType();
@@ -101,7 +100,7 @@ public class FileStoreClient extends DB {
   }
 
   /**
-   * Reads the file with the name {@code table}_{@code key}.json.
+   * Reads the file with the name {@code table}.json.
    *
    * @param table  The name of the table
    * @param key    The record key of the record to read.
@@ -114,7 +113,7 @@ public class FileStoreClient extends DB {
   public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
     String filename = getDatabaseFileName(table);
 
-    try (JsonReader jsonReader = new JsonReader(new FileReader(filename))) {
+    try (JsonReader jsonReader = getJsonReader(key, filename)) {
       Map<String, ByteIterator> values = gson.fromJson(jsonReader, valuesType);
 
       for (String field : fields) {
@@ -137,9 +136,21 @@ public class FileStoreClient extends DB {
                      Vector<HashMap<String, ByteIterator>> result) {
     String filename = getDatabaseFileName(table);
 
-    try (JsonReader jsonReader = new JsonReader(new FileReader(filename))) {
-      Map<String, ByteIterator> values = gson.fromJson(jsonReader, valuesType);
-      result.add(convertToHashMap(values, fields));
+    try {
+      List<String> strings = Files.readAllLines(Paths.get(filename),
+          Charset.forName(new FileReader(filename).getEncoding()));
+
+      int start = Integer.parseInt(startKey);
+      int lastWantedKey = start + recordCount - 1;
+      int lastPossibleKey = getKey(strings.get(strings.size() - 1));
+
+      int lastKeyToScan = lastPossibleKey > lastWantedKey ? lastWantedKey : lastPossibleKey;
+
+      for (int i = start; i == lastKeyToScan; i++) {
+        JsonReader jsonReader = getJsonReader(String.valueOf(i), filename);
+        Map<String, ByteIterator> values = gson.fromJson(jsonReader, valuesType);
+        result.add(convertToHashMap(values, fields));
+      }
 
       return Status.OK;
     } catch (IOException e) {
@@ -153,7 +164,8 @@ public class FileStoreClient extends DB {
   public Status update(String table, String key, Map<String, ByteIterator> values) {
     String filename = getDatabaseFileName(table);
 
-    try (JsonReader jsonReader = new JsonReader(new FileReader(filename))) {
+    try (JsonReader jsonReader = getJsonReader(key, filename)) {
+
       Map<String, ByteIterator> map = gson.fromJson(jsonReader, valuesType);
 
       for (String valuesKey : values.keySet()) {
@@ -183,6 +195,7 @@ public class FileStoreClient extends DB {
         fileWriter = new FileWriter(filename);
         fileWriterMap.put(table, fileWriter);
       }
+      fileWriter.write(getKeyString(key));
       fileWriter.write(output);
       fileWriter.write("\n");
       fileWriter.flush();
@@ -192,6 +205,32 @@ public class FileStoreClient extends DB {
     }
 
     return Status.ERROR;
+  }
+
+  private JsonReader getJsonReader(String key, String filename) throws IOException {
+    FileReader fileReader = new FileReader(filename);
+    List<String> components = Files.readAllLines(Paths.get(filename), Charset.forName(fileReader.getEncoding()));
+    String desiredComponent = "";
+    for (String component : components) {
+      String keyString = getKeyString(key);
+      if (component.startsWith(keyString)) {
+        desiredComponent = component.substring(keyString.length());
+      }
+    }
+
+    return new JsonReader(new StringReader(desiredComponent));
+  }
+
+  private String getKeyString(String key) {
+    return KEY_IDENTIFIER + "-" + key + "-";
+  }
+
+  private int getKey(String documentValue) {
+    String result = documentValue.replaceAll("-", "");
+    result = result.substring(KEY_IDENTIFIER.length());
+    result = result.split("\\{")[0];
+
+    return Integer.valueOf(result);
   }
 
   @Override
