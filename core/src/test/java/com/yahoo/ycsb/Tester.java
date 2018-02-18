380fc3aaa930cefad7c363d031a1577adf317ac2
ChristianNavolskyi <c.navolskyi@gmail.com>
2018-02-18 16:56:31 +0100

    Updated date on copyright

---
+++ core/src/test/java/com/yahoo/ycsb/Tester.java
@@ -0,0 +1,16 @@
+package com.yahoo.ycsb;
+
+import org.testng.annotations.Test;
+
+import java.io.*;
+import java.util.Properties;
+
+public class Tester {
+
+  @Test
+  public void test() {
+
+
+  }
+
+}
