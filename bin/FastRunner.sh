3a04d50ced0f9fc5e98043bccc58335f6131581f
ChristianNavolskyi <c.navolskyi@gmail.com>
2018-04-24 14:17:40 +0200

    Fixed scripts.

--- bin/FastRunner.sh
+++ bin/FastRunner.sh
@@ -2,9 +2,7 @@
 
 ./multi-db-runner.sh -w $1 \
 -o ~/Documents/BachelorThesis/results \
--d neo4j -f neo4j.path \
--d orientdb -f orientdb.url
--d apachejena -f outputdirectory \
+-d orientdb -f orientdb.url \
 -d sparksee -f sparksee.path \
 -l ~/Documents/HdrHistogram/HistogramLogProcessor \
 -t 3
--- bin/multi-db-runner.sh
+++ bin/multi-db-runner.sh
@@ -92,35 +92,32 @@ while getopts ':d:f:hl:o:p:s:t:w:' option; do
 done
 shift $((OPTIND - 1))
 
-CURRENT_PROGRESS=0
 function progress()
 {
     PARAM_PROGRESS=$1;
     PARAM_PHASE=$2;
 
-    if [ ${CURRENT_PROGRESS} -le 0 -a ${PARAM_PROGRESS} -ge 0 ]    ; then echo -ne "[..........................] (0%)  $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 5 -a ${PARAM_PROGRESS} -ge 5 ]    ; then echo -ne "[#.........................] (5%)  $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 10 -a ${PARAM_PROGRESS} -ge 10 ]  ; then echo -ne "[###.......................] (10%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 15 -a ${PARAM_PROGRESS} -ge 15 ]  ; then echo -ne "[####......................] (15%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 20 -a ${PARAM_PROGRESS} -ge 20 ]  ; then echo -ne "[#####.....................] (20%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 25 -a ${PARAM_PROGRESS} -ge 25 ]  ; then echo -ne "[#######...................] (25%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 30 -a ${PARAM_PROGRESS} -ge 30 ]  ; then echo -ne "[########..................] (30%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 35 -a ${PARAM_PROGRESS} -ge 35 ]  ; then echo -ne "[#########.................] (35%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 40 -a ${PARAM_PROGRESS} -ge 40 ]  ; then echo -ne "[##########................] (40%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 45 -a ${PARAM_PROGRESS} -ge 45 ]  ; then echo -ne "[############..............] (45%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 50 -a ${PARAM_PROGRESS} -ge 50 ]  ; then echo -ne "[#############.............] (50%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 55 -a ${PARAM_PROGRESS} -ge 55 ]  ; then echo -ne "[##############............] (55%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 60 -a ${PARAM_PROGRESS} -ge 60 ]  ; then echo -ne "[################..........] (60%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 65 -a ${PARAM_PROGRESS} -ge 65 ]  ; then echo -ne "[#################.........] (65%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 70 -a ${PARAM_PROGRESS} -ge 70 ]  ; then echo -ne "[##################........] (70%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 75 -a ${PARAM_PROGRESS} -ge 75 ]  ; then echo -ne "[####################......] (75%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 80 -a ${PARAM_PROGRESS} -ge 80 ]  ; then echo -ne "[#####################.....] (80%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 85 -a ${PARAM_PROGRESS} -ge 85 ]  ; then echo -ne "[######################....] (85%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 90 -a ${PARAM_PROGRESS} -ge 90 ]  ; then echo -ne "[#######################...] (90%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 95 -a ${PARAM_PROGRESS} -ge 95 ]  ; then echo -ne "[#########################.] (95%) $PARAM_PHASE \033[0K\r" ; fi;
-    if [ ${CURRENT_PROGRESS} -le 100 -a ${PARAM_PROGRESS} -ge 100 ]; then echo -e "$PARAM_PHASE \033[0K" ; PARAM_PROGRESS=0; fi;
-
-    CURRENT_PROGRESS=${PARAM_PROGRESS};
+    if [ ${PARAM_PROGRESS} -ge 0 ]; then echo -ne "[..........................] ($PARAM_PROGRESS%)  $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 5 ]; then echo -ne "[#.........................] ($PARAM_PROGRESS%)  $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 10 ]; then echo -ne "[###.......................] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 15 ]; then echo -ne "[####......................] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 20 ]; then echo -ne "[#####.....................] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 25 ]; then echo -ne "[#######...................] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 30 ]; then echo -ne "[########..................] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 35 ]; then echo -ne "[#########.................] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 40 ]; then echo -ne "[##########................] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 45 ]; then echo -ne "[############..............] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 50 ]; then echo -ne "[#############.............] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 55 ]; then echo -ne "[##############............] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 60 ]; then echo -ne "[################..........] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 65 ]; then echo -ne "[#################.........] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 70 ]; then echo -ne "[##################........] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 75 ]; then echo -ne "[####################......] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 80 ]; then echo -ne "[#####################.....] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 85 ]; then echo -ne "[######################....] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 90 ]; then echo -ne "[#######################...] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 95 ]; then echo -ne "[#########################.] ($PARAM_PROGRESS%) $PARAM_PHASE \033[0K\r" ; fi;
+    if [ ${PARAM_PROGRESS} -ge 100 ]; then echo -e "$PARAM_PHASE \033[0K" ; PARAM_PROGRESS=0; fi;
 }
 
 # Check if workload folder is set and is a directory.
